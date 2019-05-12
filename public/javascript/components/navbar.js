59c5d46a0004db92293d098c753d2f83d2c53bea
jordi Tabit <jordi.tabit@hotmail.fr>
2019-05-12 12:26:41 +0200

    navbar dynamic classes

--- public/javascript/components/navbar.js
+++ public/javascript/components/navbar.js
@@ -1,5 +1,10 @@
 const initNavbar = () => {
-  console.log('coucou1');
+  const banner = document.querySelector('.banner');
+  if (banner == null) {
+  const navbar = document.querySelector('.navbar-ch');
+  navbar.classList.remove('navbar-absolute');
+  navbar.classList.add('navbar-background');
+  };
 }
 
 export default initNavbar;
