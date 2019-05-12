238990e009bfa3e4774e33392a8efdb51dd83c6c
jordi Tabit <jordi.tabit@hotmail.fr>
2019-05-12 16:12:21 +0200

    add smooth scrolling on home page

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,9 @@
 import initNavbar from './components/navbar.js';
 import initComments from './components/comments.js';
 import initAuthor from './components/author.js';
+import initSmoothAnchorLink from './components/anchors.js'
 
 initNavbar();
 initComments();
 initAuthor();
+initSmoothAnchorLink();
